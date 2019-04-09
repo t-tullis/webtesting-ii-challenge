ff66f2d28ccbe9bd53866f01e73b785f86f41266
t-tullis <t.tullis34@gmail.com>
2019-04-09 16:12:06 -0700

    removed unnecessary test files and added some testing to app.test.js

--- baseballapp/src/App.test.js
+++ baseballapp/src/App.test.js
@@ -1,9 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render, fireEvent } from 'react-testing-library'
 import App from './App';
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders successfully', () => {
+  render(<App />);
+});
+
+it('adds a strike', () => {
+  const { getByText } = render(<App />);
+
+  const button = getByText('Strike');
+
+  fireEvent.click(button);
+  fireEvent.click(button);
+  
+  getByText(/Strikes: 2/i);
+});
+
+it('adds a ball', () => {
+  const { getByText } = render(<App />);
+
+  const button = getByText('Ball');
+
+  fireEvent.click(button);
+  fireEvent.click(button);
+  fireEvent.click(button);
+
+  getByText(/Balls: 3/i)
+})
+
+it('records a hit', () => {
+  const { getByText } = render(<App />);
+
+  const button = getByText('Hit');
+
+  fireEvent.click(button);
+ 
+  getByText(/Strikes: 0/i)
+  getByText(/Balls: 0/i)
+})
--- baseballapp/src/components/Display.js
+++ baseballapp/src/components/Display.js
@@ -32,7 +32,6 @@ class Display extends Component {
   }else{
     this.resetCount()
   }
-
 }
 
   ball = () => {
